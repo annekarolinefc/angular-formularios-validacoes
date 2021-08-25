80b2dc4cf3dc70ab81ea4845ec9309f970f9d1a6
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2021-08-25 13:51:01 -0300

    Trabalhando com formulários e Validações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formularios';
+
+  myForm = new FormGroup({
+    name: new FormControl('', [Validators.required, forbiddenNameValidator("Danilo")]),
+    address: new FormControl(''),
+  });
+
+  onSubmit(){
+    console.log(this.myForm.value)
+  }
+}
+
+//PROIBINDO O NOME DANILO
+export function forbiddenNameValidator(invalidName: string): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    if(control.value === invalidName){
+      return {forbiddenName: {value: control.value}}
+    }else{
+      return null;
+    }
+  };
 }
